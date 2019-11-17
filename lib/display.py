1ff4d613aa88f26c376db0a0041f3fe33884307a
nebriv <ben@benvirgilio.com>
2019-11-17 11:54:00 -0500

    Testing new display output

--- lib/display.py
+++ lib/display.py
@@ -25,6 +25,8 @@ fh.setFormatter(formatter)
 # add handler to logger object
 logger.addHandler(fh)
 
+def do_nothing(obj):
+    pass
 
 class Display:
     RST = 24
@@ -34,6 +36,7 @@ class Display:
         if import_success:
             serial = i2c(port=1, address=0x3C)
             self.disp = ssd1306(serial, rotate=0, height=32, width=128)
+            self.disp.cleanup = do_nothing()
             self.disp.clear()
         else:
             logger.warning("No display modules found, running in dummy mode")
@@ -73,52 +76,66 @@ class Display:
 
         if message is None:
             message = []
-        if import_success:
-            width = self.disp.width
-            height = self.disp.height
-            image = Image.new('1', (width, height))
-            draw = ImageDraw.Draw(image)
-            draw.rectangle((0, 0, width, height), outline=0, fill=0)
-            # Draw some shapes.
-            # First define some constants to allow easy resizing of shapes.
-            padding = 1
-            shape_width = 20
-            top = padding
-            bottom = height - padding
-            # Move left to right keeping track of the current x position for drawing shapes.
-            x = padding
-            # Draw an ellipse.
-            # draw.ellipse((x, top , x+shape_width, bottom), outline=255, fill=0)
-            # x += shape_width+padding
-            # Draw a rectangle.
-            # draw.rectangle((x, top, x+shape_width, bottom), outline=255, fill=0)
-            # x += shape_width+padding
-            # Draw a triangle.
-            # draw.polygon([(x, bottom), (x+shape_width/2, top), (x+shape_width, bottom)], outline=255, fill=0)
-            # x += shape_width+padding
-            # Draw an X.
-            # draw.line((x, bottom, x+shape_width, top), fill=255)
-            # draw.line((x, top, x+shape_width, bottom), fill=255)
-            # x += shape_width+padding
-            # Load default font.
-            font = ImageFont.load_default()
-            # Write two lines of text.
-            line_break = 0
-            formatted_lines = []
-            # line length
-            n = 100
-            for line in message:
-                formatted_lines += wrap(line, 20)
-                # print(formatted_lines)
-            print(formatted_lines)
 
+        formatted_lines = []
+        for line in message:
+            formatted_lines += wrap(line, 20)
+            # print(formatted_lines)
+        with canvas(self.disp) as draw:
+            draw.rectangle(self.disp.bounding_box, outline="white", fill="black")
+            y = 0
+            x = 3
             for line in formatted_lines:
-                draw.text((x, top + line_break), line, font=font, fill=200)
-                line_break = 10
-            # Display image.
-            # self.disp.image(image)
-            if self.on:
-                self.disp.display(image)
+                draw.text((x,y), line, fill="white")
+                y += 10
+
+
+        # if import_success:
+        #     width = self.disp.width
+        #     height = self.disp.height
+        #     image = Image.new('1', (width, height))
+        #     draw = ImageDraw.Draw(image)
+        #     draw.rectangle((0, 0, width, height), outline=0, fill=0)
+        #     # Draw some shapes.
+        #     # First define some constants to allow easy resizing of shapes.
+        #     padding = 1
+        #     shape_width = 20
+        #     top = padding
+        #     bottom = height - padding
+        #     # Move left to right keeping track of the current x position for drawing shapes.
+        #     x = padding
+        #     # Draw an ellipse.
+        #     # draw.ellipse((x, top , x+shape_width, bottom), outline=255, fill=0)
+        #     # x += shape_width+padding
+        #     # Draw a rectangle.
+        #     # draw.rectangle((x, top, x+shape_width, bottom), outline=255, fill=0)
+        #     # x += shape_width+padding
+        #     # Draw a triangle.
+        #     # draw.polygon([(x, bottom), (x+shape_width/2, top), (x+shape_width, bottom)], outline=255, fill=0)
+        #     # x += shape_width+padding
+        #     # Draw an X.
+        #     # draw.line((x, bottom, x+shape_width, top), fill=255)
+        #     # draw.line((x, top, x+shape_width, bottom), fill=255)
+        #     # x += shape_width+padding
+        #     # Load default font.
+        #     font = ImageFont.load_default()
+        #     # Write two lines of text.
+        #     line_break = 0
+        #     formatted_lines = []
+        #     # line length
+        #     n = 100
+        #     for line in message:
+        #         formatted_lines += wrap(line, 20)
+        #         # print(formatted_lines)
+        #     print(formatted_lines)
+        #
+        #     for line in formatted_lines:
+        #         draw.text((x, top + line_break), line, font=font, fill=200)
+        #         line_break = 5
+        #     # Display image.
+        #     # self.disp.image(image)
+        #     if self.on:
+        #         self.disp.display(image)
         else:
             logger.error("Import failed. Missing modules.")
             pass
