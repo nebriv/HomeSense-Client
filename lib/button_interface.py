c8ff24ae2158d1df584b8e55adc3421456830515
nebriv <ben@benvirgilio.com>
2019-11-17 13:27:50 -0500

    Bug fix

--- lib/button_interface.py
+++ lib/button_interface.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 import time
-import display
-import device_manage
+from . import display
+from . import device_manage
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
